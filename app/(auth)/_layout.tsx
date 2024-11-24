0c07aabe0e1eabf63ff32cb5b8e3e3621700a700
Christian Jhegg Fermilan <christianjheggfer@gmail.com>
2024-11-24 19:14:18 +0800

    fix: android layout

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -4,12 +4,19 @@ import {
   ScrollView,
   Platform,
   KeyboardAvoidingView,
+  StatusBar
 } from "react-native";
 import { Slot } from "expo-router";
 
 export default function AuthLayout() {
   return (
-    <SafeAreaView className="flex-1 bg-white">
+    <SafeAreaView
+      className="flex-1 bg-white"
+      style={{
+        // Add padding top only for Android when StatusBar is translucent
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+      }}
+    >
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
         className="flex-1"
