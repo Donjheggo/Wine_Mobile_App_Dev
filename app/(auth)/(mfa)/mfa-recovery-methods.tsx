3717fe602b0ad6865edd49ca4d2ca9f245af2303
Christian Jhegg Fermilan <christianjheggfer@gmail.com>
2024-11-24 18:51:28 +0800

    feat: Add mfa recovery email method screen

--- app/(auth)/(mfa)/mfa-recovery-methods.tsx
+++ app/(auth)/(mfa)/mfa-recovery-methods.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { View, TouchableOpacity } from "react-native";
 import { Text } from "~/components/ui/text";
 import { RectangleEllipsis } from "~/lib/icons/rectangle-ellipsis";
 import { Mail } from "~/lib/icons/mail";
@@ -16,30 +16,34 @@ export default function Screen() {
         since the 1500s
       </Text>
       <Link href="/" asChild>
-        <View className="mt-14 flex-row items-center gap-2">
-          <RectangleEllipsis
-            size={45}
-            strokeWidth={1}
-            className="text-muted-foreground"
-          />
-          <View>
-            <Text>Recovery code</Text>
-            <Text className="text-sm text-muted-foreground">
-              Recover using 6 digit code from authenticator.
-            </Text>
+        <TouchableOpacity>
+          <View className="mt-14 flex-row items-center gap-2">
+            <RectangleEllipsis
+              size={45}
+              strokeWidth={1}
+              className="text-muted-foreground"
+            />
+            <View>
+              <Text>Recovery code</Text>
+              <Text className="text-sm text-muted-foreground">
+                Recover using 6 digit code from authenticator.
+              </Text>
+            </View>
           </View>
-        </View>
+        </TouchableOpacity>
       </Link>
-      <Link href="/" asChild>
-        <View className="mt-10 flex-row items-center gap-2">
-          <Mail size={45} strokeWidth={1} className="text-muted-foreground" />
-          <View>
-            <Text>Email</Text>
-            <Text className="text-sm text-muted-foreground">
-              Recover using your email address.
-            </Text>
+      <Link href="/(auth)/(mfa)/mfa-recovery-email-method" asChild>
+        <TouchableOpacity>
+          <View className="mt-10 flex-row items-center gap-2">
+            <Mail size={45} strokeWidth={1} className="text-muted-foreground" />
+            <View>
+              <Text>Email</Text>
+              <Text className="text-sm text-muted-foreground">
+                Recover using your email address.
+              </Text>
+            </View>
           </View>
-        </View>
+        </TouchableOpacity>
       </Link>
     </View>
   );
