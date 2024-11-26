2c1cbb9f126a630f64678a3ee5af7b6d35d8aa7e
Christian Jhegg Fermilan <christianjheggfer@gmail.com>
2024-11-26 19:10:33 +0800

    feat: add settings password screen

--- app/(tabs)/settings/password.tsx
+++ app/(tabs)/settings/password.tsx
@@ -1,10 +1,12 @@
 import { View } from "react-native";
 import { Text } from "~/components/ui/text";
+import EditPasswordForm from "~/components/settings/password/edit-password-form";
 
 export default function Screen() {
   return (
     <View>
-      <Text>Password Settings</Text>
+      <Text className="text-xl font-poppins-medium">Password</Text>
+      <EditPasswordForm />
     </View>
   );
 }
---
+++ components/settings/password/edit-password-form.tsx
@@ -0,0 +1,94 @@
+import { View } from "react-native";
+import { Input } from "../../ui/input";
+import { useState } from "react";
+import { Button } from "../../ui/button";
+import { Text } from "../../ui/text";
+import { Pencil } from "~/lib/icons/pencil";
+
+export default function EditPasswordForm() {
+  const [editing, setEditing] = useState<boolean>(false);
+  const [form, setForm] = useState<{
+    current_password: string;
+    new_password: string;
+    confirm_new_password: string;
+  }>({
+    current_password: "",
+    new_password: "",
+    confirm_new_password: "",
+  });
+
+  const handleSubmit = async () => {
+    try {
+      // add function for editing
+      setEditing(false);
+    } catch (error) {
+      console.error(error);
+    }
+    return;
+  };
+
+  return (
+    <View className="mt-4">
+      {!editing ? (
+        <View className="flex-row items-center justify-between">
+          <View>
+            <Text className="text-xl">Password</Text>
+            <Text className="text-muted-foreground text-lg">*********</Text>
+          </View>
+          <Pencil
+            onPress={() => setEditing(true)}
+            className="text-muted-foreground"
+          />
+        </View>
+      ) : (
+        // Form here
+        <View className="gap-2 mt-6">
+          <View>
+            <View className="flex-row justify-between items-center">
+              <Text className="text-xl">Current Password</Text>
+              <Button
+                onPress={() => setEditing(false)}
+                variant="ghost"
+                size="sm"
+              >
+                <Text>Cancel</Text>
+              </Button>
+            </View>
+            <Input
+              value={form.current_password}
+              onChangeText={(e) => setForm({ ...form, current_password: e })}
+              keyboardType="default"
+              secureTextEntry={true}
+              placeholder="***********"
+            />
+          </View>
+          <View className="gap-2">
+            <Text className="text-xl">New Password</Text>
+            <Input
+              value={form.new_password}
+              onChangeText={(e) => setForm({ ...form, new_password: e })}
+              keyboardType="default"
+              secureTextEntry={true}
+              placeholder="***********"
+            />
+          </View>
+          <View className="gap-2">
+            <Text className="text-xl">Confirm Password</Text>
+            <Input
+              value={form.confirm_new_password}
+              onChangeText={(e) =>
+                setForm({ ...form, confirm_new_password: e })
+              }
+              keyboardType="default"
+              secureTextEntry={true}
+              placeholder="***********"
+            />
+          </View>
+          <Button onPress={handleSubmit} className="w-32">
+            <Text>Save</Text>
+          </Button>
+        </View>
+      )}
+    </View>
+  );
+}
