b647dbbfb62fa8faf80dd83b64988e5465c1e282
Christian Jhegg Fermilan <christianjheggfer@gmail.com>
2024-11-29 02:42:21 +0800

    fix: auth layout

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,13 +1,15 @@
 import { Slot } from "expo-router";
 import ScreenLayout from "~/components/layout/screen-layout";
-import { View } from "react-native";
+import { View, ScrollView } from "react-native";
 
 export default function ScreensLayout() {
   return (
     <ScreenLayout>
-      <View className="container max-w-md mx-auto">
-        <Slot />
-      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View className="container max-w-md mx-auto">
+          <Slot />
+        </View>
+      </ScrollView>
     </ScreenLayout>
   );
 }
